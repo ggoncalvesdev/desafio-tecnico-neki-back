cf27cdf47f3a1b2d73673842d8fca49e43f39ee8
Gabriel Gonçalves <92227976+ggoncalvesdev@users.noreply.github.com>
2023-01-03 14:45:14 -0300

    feat(Initial):Config

---
+++ src/main/java/com/neki/NekiApplication.java
@@ -0,0 +1,12 @@
+package com.neki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NekiApplication.class, args);
+  }
+}
